f1c98b4cda99d8d687f17fea8c19f8f2016b618f
ElormLokpo <benedictdev31@gmail.com>
2025-07-30 13:59:54 +0000

    switching backend

--- backend/src/main/java/app/membership/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package app.membership.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/app/membership/models/enums/RolesEnum.java
+++
@@ -1,7 +0,0 @@
-package app.membership.models.enums;
-
-public enum RolesEnum {
-    ROLE_ADMIN,
-    ROLE_FRONTDESK,
-    ROLE_MEMBER
-}
